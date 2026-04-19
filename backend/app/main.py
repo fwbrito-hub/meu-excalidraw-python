d9ff8518ffa8683b3f08bbaba70daf4747e128db
Chico Brito <chicobrito908@gmail.com>
2026-04-19 11:58:37 -0300

    chore: configurações de deploy para o Railway e variável VITE_API_URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,12 +42,8 @@ def startup_event():
 # 2. Configurando CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
